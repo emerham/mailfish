265dedd7e1fa337a6fd7302953bdaf4821fb1596
Matthew Brabham <matthew.brabham@oregonstate.edu>
2017-01-27 13:15:24 -0800

    updated theming and started using a template file for the block

---
+++ mailfish-block.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brabhamm
+ * Date: 1/27/17
+ * Time: 1:10 PM
+ */
+?>
+<div id='mailfish-rocks'>
+  <?php print t('Mailfish Rocks'); ?>
+  <?php print $form; ?>
+</div>
